e9cce7e0d7da09bd27760bb79425be3037281ad2
Eugene488 <69659012+Eugene488@users.noreply.github.com>
2020-08-21 10:39:49 +0800

    Revert "Merge branch 'map-building' into betamaster"
    This reverts commit bfe01fe88c7966722fffcaf9b3664a1ceb364076, reversing
    changes made to 64e82e3261d829f4353a6400ff89e511f31d4d96.

--- SP1Framework/game.h
+++ SP1Framework/game.h
@@ -99,8 +99,6 @@ void maskrenderout();
 void renderMask();
 void renderOver(); // render Game Over
 void updateOver(); // update over
-void mapchange(int x);
-
 // keyboard and mouse input event managers
 void keyboardHandler(const KEY_EVENT_RECORD& keyboardEvent);  // define this function for the console to call when there are keyboard events
 void mouseHandler(const MOUSE_EVENT_RECORD& mouseEvent);      // define this function for the console to call when there are mouse events
